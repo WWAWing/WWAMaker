b3ec040c45f543a89be219afd84c7f8d9a8474ae
Aokashi <mail@aokashi.net>
2016-08-04 00:03:39 +0900

    WWAMkクラスや定数を設定しました、画像もアップロード

--- src/wwamk_main.ts
+++ src/wwamk_main.ts
@@ -1,9 +1,55 @@
 /// <reference path="wwamk_parts.ts" />
 
 const EXTRACTING_MAPDATA_FILENAME:string = "wwamap.dat"; // 吸い出すファイル名
+const MAP_WIDTH_MAXIMUM:number = 1001;
+const OBJECT_PARTS_MAXIMUM:number = 4000;
+const MAP_PARTS_MAXIMUM:number = 4000;
+
+// データ読み込みの定数
+const CHIP_WIDTH: number = 40; // 1マスの横幅
+const CHIP_HEIGHT: number = 40; // 1マスの縦幅
+const PARTS_ATTRIBUTE_NUMBER_ATTRIBUTE: number = 3; // Attribute配列の各配列のうち、属性を示す場所
+const PARTS_ATTRIBUTE_NUMBER_MESSAGE: number = 5; // Attribute配列の各配列のうち、メッセージを示す場所
+const PARTS_ATTRIBUTE_NUMBER_IMAGE_X: number = 6; // Attribute配列の各配列のうち、画像X位置を示す場所
+const PARTS_ATTRIBUTE_NUMBER_IMAGE_Y: number = 7; // Attribute配列の各配列のうち、画像Y座標を示す場所
+const PARTS_ATTRIBUTE_START_PARAMETERS: number = 10; // Attribute配列の各配列のうち、パラメータを示す開始場所
+const PARTS_ATTRIBUTE_START_APPEAR: number = 20; // Attribute配列の各配列のうち、指定位置のパーツ出現を示す開始場所
 
 var t_start: number; // 読み込み開始時間
 var t_end: number; // 読み込み完了時間
+
+class WWAMk{
+    worldName: string;
+    mapCGName: string;
+    playerX: number;
+    playerY: number;
+    gameoverX: number;
+    gameoverY: number;
+    statusEnergyMax: number;
+    statusEnergy: number;
+    statusStrength: number;
+    statusDefence: number;
+    statusGold: number;
+    mapWidth: number;
+    objectPartsMax: number;
+    mapPartsMax: number;
+    systemMessage: SystemMessage;
+    objectMap: number[][] = new Array(MAP_WIDTH_MAXIMUM);
+    mapMap: number[][] = new Array(MAP_WIDTH_MAXIMUM);
+    objectParts:ObjectParts[] = new Array(OBJECT_PARTS_MAXIMUM);
+    mapParts:MapParts[] = new Array(MAP_PARTS_MAXIMUM);
+}
+
+class SystemMessage{
+    linkMessage: string;
+    notHaveGoldMessage: string;
+    notHaveItemMessage: string;
+    useItemMessage: string;
+    getClickableItemMessage: string;
+    limitItemMessage: string;
+    soundMessage: string;
+}
+
 var messageHandler = function (e) {
     if (e.data.error !== null && e.data.error !== void 0) {
         try {
