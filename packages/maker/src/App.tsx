9538f7610c5bc6b8a3a2c175fac944c98846e1d5
Aokashi <Aokashi.Towa@Gmail.com>
2019-05-11 18:58:17 +0900

    MainEditAreaを廃止し、レイアウトの役目をAppコンポーネントに移行

--- packages/maker/src/App.tsx
+++ packages/maker/src/App.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
+import styles from './App.module.scss';
 import MainToolbar from './components/MainToolbar';
-import MainEditArea from './components/MainEditArea';
+import ToolPanel from './components/ToolPanel';
+import InfoPanel from './components/InfoPanel';
+import MapView from './components/MapView';
 
 /**
  * WWA Maker 全体のコンポーネントです。
+ * この中に各パネルと MapView が含まれていて、これらの要素のレイアウトを行う役割を持ちます。
  */
 export default class App extends React.Component {
-  public render() {
-    return (
-      <div>
-        <MainToolbar></MainToolbar>
-        <MainEditArea></MainEditArea>
-      </div>
-    );
-  }
+    public render() {
+        return (
+            <div className={styles.app}>
+                <div className={styles.mainToolbar}>
+                    <MainToolbar></MainToolbar>
+                </div>
+                <div className={styles.mapView}>
+                    <MapView></MapView>
+                </div>
+                <div className={styles.infoPanel}>
+                    <InfoPanel></InfoPanel>
+                </div>
+                <div className={styles.toolPanel}>
+                    <ToolPanel></ToolPanel>
+                </div>
+            </div>
+        );
+    }
 }
