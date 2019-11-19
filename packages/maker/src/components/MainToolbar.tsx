9ee34fc10e3fd9c2f6ad81fe630f37b42d1557c0
Aokashi <Aokashi.Towa@Gmail.com>
2019-11-19 14:27:28 +0900

    MainToolbar に読み込むマップデータを指定できるテキストボックスを追加

--- packages/maker/src/components/MainToolbar.tsx
+++ packages/maker/src/components/MainToolbar.tsx
@@ -7,14 +7,37 @@ interface Props {
     dispatch: Dispatch;
 }
 
-class MainToolbar extends React.Component<Props> {
+interface States {
+    mapdataFileName: string;
+}
+
+class MainToolbar extends React.Component<Props, States> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            mapdataFileName: ''
+        }
+    }
+
     private handleClick() {
-        this.props.dispatch(loadWWAData('wwamap.dat'));
+        this.props.dispatch(loadWWAData(this.state.mapdataFileName));
+    }
+
+    /**
+     * マップデーファイル名を変更します
+     * @param event 
+     * @todo 開く機能が実装でき次第削除する
+     */
+    private changeMapdataFileName(event: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({
+            mapdataFileName: event.target.value
+        })
     }
 
     public render() {
         return (
             <div>
+                <input type='text' value={this.state.mapdataFileName} onChange={this.changeMapdataFileName.bind(this)} />
                 <span onClick={this.handleClick.bind(this)}>open</span>
             </div>
         );
