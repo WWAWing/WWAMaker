55a5f0c6b69b63e0dd1b675dc70ef8e6873f18d4
Aokashi <Aokashi.Towa@Gmail.com>
2020-04-18 01:42:11 +0900

    cleanerのメッセージ掃除処理を変更
    現状だとすべてのメッセージが削除されてしまうため

--- packages/saver/src/cleaner/index.ts
+++ packages/saver/src/cleaner/index.ts
@@ -4,7 +4,7 @@ import getMapWidth from "./getMapWidth";
 import getPartsCount from "./getPartsCount";
 
 /**
- * WWAData から不要な文字列を取り除きます。
+ * WWAData から不要な情報を取り除き最適化します。
  * @param wwaData 
  */
 export default function clean(wwaData: WWAData): WWAData {
@@ -12,21 +12,28 @@ export default function clean(wwaData: WWAData): WWAData {
     const newWWAData = Object.assign({}, wwaData);
 
     /**
-     * 1. 使用されているメッセージのインデックスを取得します。
+     * 1. 使用されていないメッセージを削除します。
      */
-    let usedMessageIndex: number[] = [];
-    usedMessageIndex.concat(wwaData.objectAttribute.map((attribute) => attribute[WWAConsts.ATR_STRING]));
-    usedMessageIndex.concat(wwaData.mapAttribute.map((attribute) => attribute[WWAConsts.ATR_STRING]));
-    usedMessageIndex.sort();
+    const objectMessageIndex = wwaData.objectAttribute.map((attribute) => attribute[WWAConsts.ATR_STRING]);
+    const mapMessageIndex = wwaData.mapAttribute.map((attribute) => attribute[WWAConsts.ATR_STRING]);
+    /**
+     * lastItemIndex は最後のメッセージデータのインデックスで、計算を終えるとそのインデックス以降のメッセージデータは削除されます。
+     */
+    let lastItemIndex = 0;
+    newWWAData.message = wwaData.message.map((message, messageIndex) => {
+        if (objectMessageIndex.includes(messageIndex) || mapMessageIndex.includes(messageIndex)) {
+            lastItemIndex = messageIndex;
+            return message;
+        }
+        return "";
+    });
+
+    newWWAData.message.splice(lastItemIndex + 1);
+    newWWAData.messageNum = newWWAData.message.length;
+
 
     /**
-     * 2. 用意したメッセージのインデックスから、メッセージの配列を作成します。
-     */
-    newWWAData.message = usedMessageIndex.map((messageIndex) => wwaData.message[messageIndex]);
-    newWWAData.messageNum = usedMessageIndex.length;
-
-    /**
-     * 3. マップの幅を最適化します。
+     * 2. マップの幅を最適化します。
      */
     const newMapWidth = getMapWidth(wwaData.map, wwaData.mapObject);
     newWWAData.mapWidth = newMapWidth;
@@ -37,13 +44,15 @@ export default function clean(wwaData: WWAData): WWAData {
     removeMapPartsOutside(newWWAData.map, newMapWidth);
     removeMapPartsOutside(newWWAData.mapObject, newMapWidth);
 
+
     /**
-     * 4. パーツ属性を最適化します。
+     * 3. パーツ属性を最適化します。
      */
     const newMapCount = getPartsCount(wwaData.mapAttribute);
     newWWAData.mapPartsMax = newMapCount;
     const newObjectCount = getPartsCount(wwaData.objectAttribute);
     newWWAData.objPartsMax = newObjectCount;
+
 
     return newWWAData;
 }
