4929cc9de7a99520c8fc0d8351f7f05383a410ab
Aokashi <Aokashi.Towa@Gmail.com>
2020-05-09 01:14:13 +0900

    出力するクラスコンポーネントを実装

--- packages/image-decorder/src/index.ts
+++ packages/image-decorder/src/index.ts
@@ -3,12 +3,55 @@ import React from "react";
 type GetImageUrl<P> = (prps: P) => string;
 type SetImage<P> = (image: HTMLImageElement, props: P) => P;
 
-export default function imageDecorder<P>(getImageUrl: GetImageUrl<P>, setImage: SetImage<P>) {
-    return (CurrentComponent: React.Component) => {
+interface State {
+    image: HTMLImageElement | null;
+}
 
-        return class extends React.Component<P> {
+export default function imageDecorder<P>(getImageUrl: GetImageUrl<P>, setImage: SetImage<P>) {
+    return (wrappedComponent: React.FunctionComponent<P> | React.ComponentClass<P>) => {
+
+        return class extends React.Component<P, State> {
+            constructor(props: P) {
+                super(props);
+                this.state = {
+                    image: null
+                };
+            }
+
+            public componentDidMount() {
+                this.createImage();
+            }
+
+            public componentDidUpdate() {
+                this.createImage();
+            }
+
+            public shouldComponentUpdate(nextProps: P) {
+                return getImageUrl(nextProps) !== getImageUrl(this.props);
+            }
+
+            public createImage() {
+                let image = new Image();
+                image.onload = () => {
+                    this.setState({
+                        image
+                    });
+                };
+                image.onerror = () => {
+                    this.setState({
+                        image: null
+                    });
+                };
+                image.src = getImageUrl(this.props);
+            }
+
             public render() {
-                return CurrentComponent;
+                if (this.state.image === null) {
+                    return React.createElement(wrappedComponent, this.props);
+                }
+
+                const props = setImage(this.state.image, this.props);
+                return React.createElement(wrappedComponent, props);
             }
         };
 
