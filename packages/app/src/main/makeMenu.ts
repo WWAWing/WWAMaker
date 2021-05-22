162a7f8358be7a063918c25f4a5c3112f65da607
Aokashi <Aokashi.Towa@Gmail.com>
2021-05-23 00:05:40 +0900

    ファイルの開くと名前を付けて保存にイベントメソッドを付与

--- packages/app/src/main/makeMenu.ts
+++ packages/app/src/main/makeMenu.ts
@@ -1,6 +1,11 @@
-import { Menu, MenuItemConstructorOptions } from 'electron';
+import { App, dialog, Menu, MenuItemConstructorOptions } from 'electron';
 
 function makeMenu(): Menu {
+
+    const FILE_FILTERS = [
+        { name: 'WWA マップデータ', extensions: ['dat'] },
+        { name: 'すべてのファイル', extensions: ['*'] }
+    ];
 
     // FIXME: MenuItemConstructorOptions と明示的な指定が無いと型の不一致が発生する
     const template = [
@@ -8,9 +13,20 @@ function makeMenu(): Menu {
             label: 'ファイル',
             submenu: [
                 { label: '新規作成' },
-                { label: '開く' },
+                // TODO: 別の関数に移す
+                { label: '開く', click: () => dialog.showOpenDialogSync({
+                    title: 'マップデータを開く',
+                    filters: FILE_FILTERS
+                }) },
                 { label: '上書き保存' },
-                { label: '名前を付けて保存' },
+                // TODO: 別の関数に移す
+                { label: '名前を付けて保存', click: () => dialog.showSaveDialogSync({
+                    title: 'マップデータを保存',
+                    filters: FILE_FILTERS,
+                    properties: [
+                        'createDirectory'
+                    ]
+                }) },
                 { type: 'separator' },
                 { role: 'quit' }
             ] as MenuItemConstructorOptions[]
