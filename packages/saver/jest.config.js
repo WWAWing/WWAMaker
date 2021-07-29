ee7773bd91fa2060dbe64e88ebb97a8630811c99
Aokashi <Aokashi.Towa@Gmail.com>
2021-07-29 21:03:37 +0900

    ビルド済みのJSファイルからテストを除外
    TODO: そもそもテストソースファイルがコンパイルされないようにしたい

--- packages/saver/jest.config.js
+++ packages/saver/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testPathIgnorePatterns: ['/node_modules/', '/lib/', '/module/'],
   globals: {
     'ts-jest': {
       isolatedModules: true
